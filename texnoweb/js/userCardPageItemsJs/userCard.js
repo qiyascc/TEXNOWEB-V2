b721537882f9c775de625f6fda6a4e4162437bc9
Qiyas <qiyasturk@gmail.com>
2023-10-11 21:28:17 +0400

    fix

---
+++ texnoweb/js/userCardPageItemsJs/userCard.js
@@ -0,0 +1,13 @@
+class UserCardElement extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="container mt-5">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+      
+            </div>
+        </div>
+        `;
+    }
+}
+
+customElements.define('usercard-element', UserCardElement);
